d2cd167d39ca33af3a9704eb77f072c6a513b204
trepichio <trepichio@gmail.com>
2019-10-06 19:32:08 -0300

    initial commit

---
+++ pages/posts/_id.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="container">
+    <article>
+      <h1 class="title">{{post.title}}</h1>
+      <p>{{post.content}}</p>
+    </article>
+    <aside>
+      <h3>Posts you might enjoy</h3>
+      <ul>
+        <li v-for="related in relatedPosts">
+          <nuxt-link :to="{name: 'posts-id', params: {id: related.id}}">{{related.title}}</nuxt-link>
+        </li>
+      </ul>
+    </aside>
+  </div>
+</template>
+
+<script>
+export default {
+
+  name: 'post',
+  head () {
+    return {
+      title: this.post.title,
+      meta: [
+        {name: 'twitter.title', content: this.post.title},
+        {name: 'twitter.description', content: this.post.content},
+        {name: 'twitter.image', content: 'https://i.imgur.com/UYP2umJ.png'},
+        {name: 'twitter.card', content: 'summary_large_image'},
+      ],
+    }
+  },
+  data () {
+    return {
+      id: this.$route.params.id,
+      posts: this.$store.state.posts.all
+    }
+  },
+  computed: {
+    post () {
+      return this.posts.find(post => post.id === this.id);
+    },
+    relatedPosts () {
+      return this.posts.filter(post => post.id !== this.id);
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .container {
+    display: flex;
+    justify-content: space-between;
+    line-height: 1.5;
+  }
+  article * {
+    margin-bottom: 1rem;
+  }
+  aside {
+    min-width: 280px;
+    max-width: 280px;
+    padding-left: 2rem;
+  }
+  .title {
+    font-size: 2rem;
+  }
+</style>
---
+++ store/posts.js
@@ -0,0 +1,20 @@
+export const state = () => ({
+  all: [
+        {
+          id: 'hack',
+          title: 'Hacking VueJS + NuxtJS',
+          content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error fuga, deserunt nam magnam, nemo sequi perspiciatis iste voluptates libero, facilis accusamus molestias dolorum! Rerum ullam, quas sapiente, ratione temporibus sed!'
+        },
+        {
+          id: 'messing',
+          title: 'Messing around',
+          content: 'chromoparous Incarvillea accusive unmarled cataclasm smugly electress ichthyolatry barkhan unreflective reveneer bradylexia ergatoid bodymaking polygroove ornithodelphous ingraft hocco unridiculed bifoliolate bazooka resinbush unpleased casebox'
+        },
+                {
+          id: 'newpost',
+          title: 'new post',
+          content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error fuga, deserunt nam magnam, nemo sequi perspiciatis iste voluptates libero, facilis accusamus molestias dolorum! Rerum ullam, quas sapiente, ratione temporibus sed!'
+        }
+
+      ]
+})
